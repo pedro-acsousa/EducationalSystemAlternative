7a7e0533cb14cb025be44a55b7d3b8584e99d670
pedro <p.a.correia-de-sousa1@newcastle.ac.uk>
2020-12-09 15:54:37 +0000

    Firebase connection tested and working using Firestore

---
+++ src/main/java/epic/lms/FirebaseInitializer.java
@@ -0,0 +1,41 @@
+package epic.lms;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Service
+public class FirebaseInitializer {
+
+    @PostConstruct
+   public void initialize() {
+   try{
+       FileInputStream serviceAccount = new FileInputStream("./src/main/java/epic/lms/dbjson.json");
+
+       String basePath = new File("").getAbsolutePath();
+       System.out.println(basePath);
+
+       FirebaseOptions options = new FirebaseOptions.Builder()
+               .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+               .setDatabaseUrl("https://epic-learning.firebaseio.com")
+               .build();
+       FirebaseApp.initializeApp(options);
+   } catch (FileNotFoundException e) {
+       e.printStackTrace();
+   } catch (IOException e) {
+       e.printStackTrace();
+   }
+
+   }
+
+
+
+
+}

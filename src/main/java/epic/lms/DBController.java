e87b16fad081551419c19940ea4807f1d2d26d7b
pedro <p.a.correia-de-sousa1@newcastle.ac.uk>
2020-12-11 10:08:59 +0000

    Thymeleaf implemented in the project for showing of dynamic variables. Updated file paths and redirects. Updated dependencies and the DB controller to return a Modelview.

--- src/main/java/epic/lms/DBController.java
+++ src/main/java/epic/lms/DBController.java
@@ -1,8 +1,11 @@
 package epic.lms;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -11,6 +14,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -18,27 +22,35 @@ import java.util.concurrent.ExecutionException;
 @RestController
 public class DBController {
 int found=0;
+String firstname;
     @Autowired
     FirebaseService firebaseService;
 
-    @PostMapping("/login")
-    public void login(HttpServletResponse response, @RequestParam("login") String username, @RequestParam("password") String password) throws InterruptedException, ExecutionException, IOException, ScriptException, ServletException {
+    @PostMapping(value="/login")
+    public ModelAndView login(HttpSession session, HttpServletResponse response, @RequestParam("login") String username, @RequestParam("password") String password) throws InterruptedException, ExecutionException, IOException, ScriptException, ServletException {
         found=0;
+
         List<User> users = firebaseService.getUser();
         for (User user : users) {
             if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                firstname= user.getFirstname();
                 found=1;
                 break;
             } else{ found=0;}
         }
-        if (found==1){
-            response.sendRedirect("userAccountPage.html");
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("StudentDashboard.html");
 
+
+        if (found==1){
+            session.setAttribute("firstName",firstname);
+            mv.setViewName("StudentDashboard.html");
         }else{
-            response.sendRedirect("errorPage.html");
+
+            mv.setViewName("errorPage.html");
         }
 
-
+        return mv;
     }
 
 

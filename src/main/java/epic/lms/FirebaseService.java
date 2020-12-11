333b6ee09b9fb43a8117b18c05229414bf17d8a8
pedro <p.a.correia-de-sousa1@newcastle.ac.uk>
2020-12-11 13:50:22 +0000

    Implemented Sign up add user controller and changed user object. Added extra DB functionality

--- src/main/java/epic/lms/FirebaseService.java
+++ src/main/java/epic/lms/FirebaseService.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -25,6 +27,25 @@ public class FirebaseService {
             userArrayList.add(doc.toObject(User.class));
         }
         return userArrayList;
+    }
+
+    public User addUser(User userAdded) throws ExecutionException, InterruptedException, IOException {
+
+        Firestore db = FirestoreClient.getFirestore();
+
+
+        Map<String,String> newUser = new HashMap<String,String>();
+        newUser.put("username", userAdded.getUsername());
+        newUser.put("password", userAdded.getPassword());
+        newUser.put("firstname", userAdded.getFirstname());
+        newUser.put("lastname", userAdded.getLastname());
+        newUser.put("email", userAdded.getEmail());
+        newUser.put("role", userAdded.getRole());
+
+        db.collection("Users").document(userAdded.getUsername()).create(newUser);
+
+        return userAdded;
+
     }
 
 }

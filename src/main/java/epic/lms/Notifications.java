0a9d52f5d81b2c4e73b4912706b3cd85b7b24d22
pedro <p.a.correia-de-sousa1@newcastle.ac.uk>
2020-12-16 11:28:16 +0000

    Notification Receiving and Dismissal 100% functional lacking only minor adjustments.

---
+++ src/main/java/epic/lms/Notifications.java
@@ -0,0 +1,62 @@
+package epic.lms;
+
+import java.util.Date;
+
+public class Notifications {
+
+    private String content;
+    private Date date;
+    private boolean read;
+    private String sender;
+    private String recipient;
+    private int id;
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
